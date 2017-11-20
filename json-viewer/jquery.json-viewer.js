5deb2c8802c192dd7d0655955b92bb35116c1c8d
lillypad <lchalupowski@gosecure.ca>
2017-11-20 13:55:48 -0400

    update

--- json-viewer/jquery.json-viewer.js
+++ json-viewer/jquery.json-viewer.js
@@ -30,6 +30,9 @@
     if (typeof json === 'string') {
       /* Escape tags */
       json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+      if (options.withLinks == undefined){
+          options.withLinks = false;
+      }
       if (options.withLinks && isUrl(json))
         html += '<a href="' + json + '" class="json-string">' + json + '</a>';
       else
