fb410ad1cf3ae876dd1227e6ffcbba0f359c1756
Alexandre Bodelot <alexandre.bodelot@gmail.com>
2022-05-07 17:34:12 +0200

    Fixed linter offenses

--- json-viewer/jquery.json-viewer.js
+++ json-viewer/jquery.json-viewer.js
@@ -22,16 +22,16 @@
     return urlRegexp.test(string);
   }
 
- /**
-  * Return the input string html escaped
-  * @return string
-  */
+  /**
+   * Return the input string html escaped
+   * @return string
+   */
   function htmlEscape(s) {
     return s.replace(/&/g, '&amp;')
       .replace(/</g, '&lt;')
       .replace(/>/g, '&gt;')
       .replace(/'/g, '&apos;')
-      .replace(/"/g, '&quot;')
+      .replace(/"/g, '&quot;');
   }
 
   /**
@@ -42,7 +42,7 @@
     var html = '';
     if (typeof json === 'string') {
       // Escape tags and quotes
-      json = htmlEscape(json)
+      json = htmlEscape(json);
 
       if (options.withLinks && isUrl(json)) {
         html += '<a href="' + json + '" class="json-string" target="_blank">' + json + '</a>';
--- test/jquery.json-viewer.test.js
+++ test/jquery.json-viewer.test.js
@@ -42,18 +42,18 @@ test('withQuotes option', () => {
  * Tests for the presence of a script tag inside the 'json' id.
  * If none are found, the value was correctly escaped for XSS.
  */
-function test_XSS(input) {
+function hasScripts(input) {
   $('#json').jsonViewer(input);
-  scripts_in_json = $('#json script');
-  expect(scripts_in_json.length).toEqual(0);
+  const scriptsInJson = $('#json script');
+  return scriptsInJson.length > 0;
 }
 
 test('XSS in object value', () => {
   const input = {
-    key_1: '<script>alert(1)</script>'
+    'key_1': '<script>alert(1)</script>'
   };
 
-  test_XSS(input);
+  expect(hasScripts(input)).toEqual(false);
 });
 
 test('XSS in object key', () => {
@@ -61,5 +61,5 @@ test('XSS in object key', () => {
     '<script>alert(1)</script>': 'val_1'
   };
 
-  test_XSS(input);
+  expect(hasScripts(input)).toEqual(false);
 });
